723e0538a629f6a2c693cbc7e2391d7a49c737aa
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-20 18:54:12 +0300

    add employee entity

---
+++ src/main/java/com/ozalp/AkgunTest/entities/abstracts/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.ozalp.AkgunTest.entities.abstracts;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void markAsDeleted() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/ozalp/AkgunTest/entities/concretes/Employee.java
@@ -0,0 +1,31 @@
+package com.ozalp.AkgunTest.entities.concretes;
+
+import com.ozalp.AkgunTest.entities.abstracts.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "phone_number", unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "social_security_number", unique = true, nullable = false)
+    private String socialSecurityNumber;
+}
