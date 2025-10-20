f47b660738a5614537dcfa24d54e5583be57820c
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-20 20:18:13 +0300

    add employee repository

---
+++ src/main/java/com/ozalp/AkgunTest/dataAccess/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.ozalp.AkgunTest.dataAccess;
+
+import com.ozalp.AkgunTest.entities.concretes.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+}
