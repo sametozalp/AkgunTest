d48e801ede44b9b2996fa28de363de44b31d83d0
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-20 17:23:40 +0300

    Initial commit

---
+++ src/main/java/com/ozalp/AkgunTest/AkgunTestApplication.java
@@ -0,0 +1,13 @@
+package com.ozalp.AkgunTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkgunTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkgunTestApplication.class, args);
+	}
+
+}
